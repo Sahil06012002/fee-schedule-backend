8b3802c23dd4b27a7704e9101fd9d1149fde9fc7
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2024-08-08 15:33:36 +0530

    added compare route

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
-from routes import file_router 
+from routes.files import file_router 
 
 app = FastAPI()
 app.include_router(file_router, prefix="/files")
