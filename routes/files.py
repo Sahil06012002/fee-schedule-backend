4f43adf1e66485966da39d1e09763f30bcb03aef
ArchitGupta07 <gupta.archit01@gmail.com>
2024-08-10 22:39:51 +0530

    api documentation added

--- routes/files.py
+++ routes/files.py
@@ -21,13 +21,24 @@ async def get_file_data(table_name : str,db : Database = Depends(get_db),cmp_obj
     return {"data" : table_data}
 
 
-@file_router.post("/upload")
+@file_router.post("/upload",summary="Upload an XLSX File and Store Data in Database",
+    description=(
+        "Uploads an XLSX file, extracts data from it, and stores the extracted data "
+        "in the database. The file should be in XLSX format, and the data will be inserted "
+        "into a database table managed by the TableManager."
+    ))
 async def upload_file(file: UploadFile = File(...), table_manager_obj: TableManager = Depends(TableManager),db : Database = Depends(get_db)):
     data,table_name,file_name = await table_manager_obj.insert_table(db,file)
     return {"data" : data, "table_name" : table_name, "file_name" : file_name}
 
 
-@file_router.post("/compare/{table_name}")
+@file_router.post("/compare/{table_name}",  summary="Compare XLSX File with Existing Table Data",
+    description=(
+        "Uploads an XLSX file containing data to be compared with an existing table "
+        "in the database. The file should have a similar structure to the data in the "
+        "specified table. The endpoint compares the new file with the existing data and "
+        "returns the differences or changes found."
+    ))
 async def calculate_dif(table_name: str ,cmp_file: UploadFile = File(...),cmp_obj : Comparision = Depends(Comparision),db : Database = Depends(get_db)) : 
     print(table_name)
     response = await cmp_obj.compare(db,table_name,cmp_file)
